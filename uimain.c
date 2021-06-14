149fc83b5d1b566d5ef2a8b9ba31488bf65c803b
g1tBAp04 <djbaptiste@miners.utep.edu>
2021-06-14 13:45:09 -0600

    monday entry

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrrow = a, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -24,10 +24,14 @@ int main()
       puts("You selected square:");
       print_square(5, 5);
       break;
+    case 'a':
+      puts("You selected arrow:");
+        print_arrow(5,5);
+      break;
     case 'c':
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+	print_char_8x16(c);
       break;
     case 'q':
       puts("Bye!");
